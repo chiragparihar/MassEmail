c6892a69de4bed350e214948f999ab3a3c8849bc
chiragparihar <chirageminem6@gmail.com>
2020-06-21 19:56:53 -0400

    enable oauth

---
+++ server/config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+    googleClientID : '897311738626-17fla566e22312eovuhvlknepdim44am.apps.googleusercontent.com',
+    googleClientSecret : 'ATeL19QuxUZmAsOPKgOj1TXc'
+};
--- server/index.js
+++ server/index.js
@@ -1,9 +1,10 @@
 const express = require('express');
+const passport = require('passport');
+const googlestat = require('passport-google-oauth20').Strategy;
+const keys = require('./config/keys');
 const app = express();
 
-app.get('/',(req,res)=>{
-    res.send("hello world");
-});
+passport.use(new googlestat());
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
