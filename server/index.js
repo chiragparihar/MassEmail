e3399d98777ad0ff3b16f2ccc06551df94cf313c
chiragparihar <chirageminem6@gmail.com>
2020-06-21 13:38:06 -0400

    personal message

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/',(req,res)=>{
-    res.send("hello world");
+    res.send("chimu loves aaniku");
 });
 
 const PORT = process.env.PORT || 5000;
