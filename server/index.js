6c1f4b59dfbd141d4fc3879cdde77eae05c3f9ed
chiragparihar <chirageminem6@gmail.com>
2020-06-20 20:42:45 -0400

    server msg changed

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/',(req,res)=>{
-    res.send("hello");
+    res.send("hello world");
 });
 
 const PORT = process.env.PORT || 5000;
