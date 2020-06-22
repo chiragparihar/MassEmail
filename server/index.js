df55cdd03e703643b99a3b790f85f06b8da6d82e
chiragparihar <chirageminem6@gmail.com>
2020-06-22 01:53:10 -0400

    create routes

--- server/index.js
+++ server/index.js
@@ -1,10 +1,8 @@
 const express = require('express');
-const passport = require('passport');
-const googlestat = require('passport-google-oauth20').Strategy;
-const keys = require('./config/keys');
+
 const app = express();
 
-passport.use(new googlestat());
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
