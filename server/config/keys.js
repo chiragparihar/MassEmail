ace6016919b424de2c435a0d7b7e83caa7328897
chiragparihar <chirageminem6@gmail.com>
2020-06-24 22:07:40 -0400

    clear errors

--- server/config/keys.js
+++ server/config/keys.js
@@ -1,7 +1,7 @@
 module.exports = {
     googleClientID : '897311738626-17fla566e22312eovuhvlknepdim44am.apps.googleusercontent.com',
     googleClientSecret : 'ATeL19QuxUZmAsOPKgOj1TXc',
-    mongoURI:"mongodb+srv://chiragparihar:yA753631!@massemail-lbhja.mongodb.net/logs?retryWrites=true&w=majority"
+    mongoURI:"mongodb+srv://chiragparihar:yA753631@massemail.lbhja.mongodb.net/users?retryWrites=true&w=majority"
     
     
 };
