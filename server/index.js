af2d9ab88bb5f8bc1077a27aa8d430c03351a2a0
chiragparihar <chirageminem6@gmail.com>
2020-06-25 11:30:54 -0400

    database added

--- server/index.js
+++ server/index.js
@@ -2,8 +2,9 @@ const express = require('express');
 require('./services/passport')
 const app = express();
 require('./routes/authRoutes')(app);
-const moongoose = require('moongoose');
+const mongoose = require('mongoose');
+require('./models/Users')
 const keys = require('./config/keys');
-moongoose.connect(keys.mongoURI);
+mongoose.connect(keys.mongoURI);
 PORT = process.env.PORT || 5000;
 app.listen(PORT);
