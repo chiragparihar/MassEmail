d627b79d24062ff464da63a42d736feacee1acdb
chiragparihar <chirageminem6@gmail.com>
2020-06-28 21:30:19 -0400

    started adding actions

--- server/client/src/index.js
+++ server/client/src/index.js
@@ -4,6 +4,7 @@ import ReactDOM from 'react-dom';
 
 import {Provider} from 'react-redux';
 import {createStore, applyMiddleware} from 'redux';
+import reduxThunk from 'redux-thunk';
 import App from './components/App';
 import reducers from './reducers';
 
